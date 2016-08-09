9e2b4606beddbf185e3232735cdb459c84450e0e
MRixen <ManLeo.Rixen@gmail.com>
2016-08-09 16:16:03 +0200

    Add possibility to test dynamixels
    - Add position reached checking

--- WriteToComPort/Form1.Designer.cs
+++ WriteToComPort/Form1.Designer.cs
@@ -38,13 +38,21 @@
             this.textBox_PortName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox_repetitions = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_motorSpeed = new System.Windows.Forms.TextBox();
+            this.textBox_information = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(174, 107);
+            this.button1.Location = new System.Drawing.Point(232, 132);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Turn motor";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,9 +60,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(93, 43);
+            this.button3.Location = new System.Drawing.Point(124, 53);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(100, 28);
             this.button3.TabIndex = 2;
             this.button3.Text = "Open port";
             this.button3.UseVisualStyleBackColor = true;
@@ -62,9 +71,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(174, 43);
+            this.button4.Location = new System.Drawing.Point(232, 53);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(100, 28);
             this.button4.TabIndex = 3;
             this.button4.Text = "Close port";
             this.button4.UseVisualStyleBackColor = true;
@@ -72,16 +82,19 @@
             // 
             // textBox_motorAngle
             // 
-            this.textBox_motorAngle.Location = new System.Drawing.Point(93, 107);
+            this.textBox_motorAngle.Location = new System.Drawing.Point(125, 117);
+            this.textBox_motorAngle.Margin = new System.Windows.Forms.Padding(4);
             this.textBox_motorAngle.Name = "textBox_motorAngle";
-            this.textBox_motorAngle.Size = new System.Drawing.Size(75, 20);
+            this.textBox_motorAngle.Size = new System.Drawing.Size(99, 22);
             this.textBox_motorAngle.TabIndex = 4;
+            this.textBox_motorAngle.Text = "0";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(174, 150);
+            this.button2.Location = new System.Drawing.Point(232, 185);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 5;
             this.button2.Text = "Turn right";
             this.button2.UseVisualStyleBackColor = true;
@@ -89,9 +102,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(12, 150);
+            this.button5.Location = new System.Drawing.Point(16, 185);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(100, 28);
             this.button5.TabIndex = 6;
             this.button5.Text = "Turn left";
             this.button5.UseVisualStyleBackColor = true;
@@ -99,9 +113,10 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(93, 150);
+            this.button6.Location = new System.Drawing.Point(124, 185);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.Size = new System.Drawing.Size(100, 28);
             this.button6.TabIndex = 7;
             this.button6.Text = "Turn middle";
             this.button6.UseVisualStyleBackColor = true;
@@ -109,34 +124,113 @@
             // 
             // textBox_PortName
             // 
-            this.textBox_PortName.Location = new System.Drawing.Point(93, 17);
+            this.textBox_PortName.Location = new System.Drawing.Point(124, 21);
+            this.textBox_PortName.Margin = new System.Windows.Forms.Padding(4);
             this.textBox_PortName.Name = "textBox_PortName";
-            this.textBox_PortName.Size = new System.Drawing.Size(75, 20);
+            this.textBox_PortName.Size = new System.Drawing.Size(99, 22);
             this.textBox_PortName.TabIndex = 8;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 20);
+            this.label1.Location = new System.Drawing.Point(39, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.Size = new System.Drawing.Size(77, 17);
             this.label1.TabIndex = 9;
             this.label1.Text = "Port name:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 112);
+            this.label2.Location = new System.Drawing.Point(29, 119);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.Size = new System.Drawing.Size(87, 17);
             this.label2.TabIndex = 10;
             this.label2.Text = "Motor angle:";
             // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(16, 221);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 28);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Start Test";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_StartTest_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(123, 221);
+            this.button8.Margin = new System.Windows.Forms.Padding(4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 28);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Stopp Test";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox_repetitions
+            // 
+            this.textBox_repetitions.Location = new System.Drawing.Point(123, 259);
+            this.textBox_repetitions.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_repetitions.Name = "textBox_repetitions";
+            this.textBox_repetitions.Size = new System.Drawing.Size(100, 22);
+            this.textBox_repetitions.TabIndex = 13;
+            this.textBox_repetitions.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 259);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Repetitions:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 145);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Motor speed:";
+            // 
+            // textBox_motorSpeed
+            // 
+            this.textBox_motorSpeed.Location = new System.Drawing.Point(125, 143);
+            this.textBox_motorSpeed.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_motorSpeed.Name = "textBox_motorSpeed";
+            this.textBox_motorSpeed.Size = new System.Drawing.Size(99, 22);
+            this.textBox_motorSpeed.TabIndex = 15;
+            this.textBox_motorSpeed.Text = "100";
+            // 
+            // textBox_information
+            // 
+            this.textBox_information.Location = new System.Drawing.Point(13, 297);
+            this.textBox_information.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_information.Multiline = true;
+            this.textBox_information.Name = "textBox_information";
+            this.textBox_information.Size = new System.Drawing.Size(396, 124);
+            this.textBox_information.TabIndex = 17;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(266, 195);
+            this.ClientSize = new System.Drawing.Size(422, 434);
+            this.Controls.Add(this.textBox_information);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_motorSpeed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_repetitions);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox_PortName);
@@ -147,6 +241,7 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -166,6 +261,13 @@
         private System.Windows.Forms.TextBox textBox_PortName;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox_repetitions;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_motorSpeed;
+        private System.Windows.Forms.TextBox textBox_information;
     }
 }
 
--- WriteToComPort/Form1.cs
+++ WriteToComPort/Form1.cs
@@ -7,18 +7,154 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System; 
-using System.IO.Ports; 
+using System;
+using System.IO.Ports;
 using System.Text;
-using System.Diagnostics; 
+using System.Diagnostics;
 
 namespace WriteToComPort
 {
     public partial class Form1 : Form
     {
 
-        String standardPortName = "COM16";
+        String standardPortName = "COM5";
         int standardBaudRate = 9600;
+        bool isRunning = true;
+
+    byte[] position, ids, msgStart, msgEnd, speed;
+        byte[] sendData = new byte[64];
+        int[] readData = new int[64];
+        #region position- and speed-list
+        int[] positionList0 = {311,
+342,
+373,
+403,
+432,
+460,
+486,
+511,
+533,
+554,
+572,
+587,
+600,
+610,
+617,
+620,
+621,
+619,
+613,
+605,
+593,
+579,
+562,
+542,
+520,
+497,
+471,
+443,
+415,
+385,
+355,
+324,
+293,
+262,
+232,
+202,
+174,
+146,
+121,
+97,
+76,
+57,
+40,
+27,
+16,
+7,
+2,
+1,
+2,
+6,
+13,
+24,
+37,
+53,
+71,
+92,
+115,
+140,
+167,
+195,
+224,
+254,
+285,
+311};
+
+        int[] speedList0 = {63,
+128,
+192,
+256,
+320,
+384,
+448,
+512,
+576,
+640,
+704,
+768,
+832,
+896,
+960,
+1023,
+1023,
+960,
+896,
+832,
+768,
+704,
+640,
+576,
+512,
+448,
+384,
+320,
+256,
+192,
+128,
+64,
+64,
+128,
+192,
+256,
+320,
+384,
+448,
+512,
+576,
+640,
+704,
+768,
+832,
+896,
+960,
+1023,
+1023,
+960,
+896,
+832,
+768,
+704,
+640,
+576,
+512,
+448,
+384,
+320,
+256,
+192,
+128,
+64};
+        #endregion
 
         SerialPort _serialPort;
 
@@ -31,20 +167,14 @@ namespace WriteToComPort
             button2.Enabled = false;
             button5.Enabled = false;
             button6.Enabled = false;
-            _serialPort = new SerialPort(standardPortName, standardBaudRate, Parity.None, 8, StopBits.One);       
+
+            _serialPort = new SerialPort(standardPortName, standardBaudRate, Parity.None, 8, StopBits.One);
             _serialPort.Handshake = Handshake.None;
-        }
-
-        private void button_TurnMotor_Click(object sender, EventArgs e)
-        {
-            String angle = textBox_motorAngle.Text;
-
-            if (angle.Length > 0) rotateMotor(angle);
         }
 
         private void button_OpenPort_Click(object sender, System.EventArgs e)
         {
-            _serialPort.PortName = textBox_PortName.Text;
+            //_serialPort.PortName = textBox_PortName.Text;
             _serialPort.Open();
             button3.Enabled = false;
             button4.Enabled = true;
@@ -67,19 +197,81 @@ namespace WriteToComPort
             button6.Enabled = false;
         }
 
-        private void button_TurnRight_Click(object sender, System.EventArgs e)
+        private void button_StartTest_Click(object sender, EventArgs e)
         {
-            rotateMotor("30");
+            isRunning = true;
+
+            int amount = Int32.Parse(textBox_repetitions.Text);
+            for (int j = 0; j < amount; j++)
+            {
+                for (int i = 0; i < positionList0.Length; i++)
+                {
+                    if (isRunning)
+                    {
+                        setPosition(positionList0[i], speedList0[i]);
+                        Task.Delay(-1).Wait(10);
+                    }
+                    else break;
+                }
+                //Task.Delay(-1).Wait(500);
+            }
         }
 
-        private void rotateMotor(String motorAngle)
+        private void button8_Click(object sender, EventArgs e)
         {
+            isRunning = false;
+        }
+
+        private void button_TurnMotor_Click(object sender, EventArgs e)
+        {
+            int angle = Int32.Parse(textBox_motorAngle.Text);
+            int speed = Int32.Parse(textBox_motorSpeed.Text);
+
+            setPosition(angle, speed);
+        }
+
+        private void button_TurnRight_Click(object sender, System.EventArgs e)
+        {
+            setPosition(30, 100);
+        }
+
+        private void setPosition(int motorAngle, int motorSpeed)
+        {
+            textBox_information.Text = "Send next command";
             try
             {
                 if ((_serialPort.IsOpen))
-                {                  
-                        Debug.Write("Write to port" + "\n");
-                        _serialPort.Write("1;30;" + motorAngle + ";\n");
+                {
+                    // Set start of message
+                    msgStart = BitConverter.GetBytes(9999);
+                    for (int i = 0; i < msgStart.Length; i++) sendData[i] = msgStart[i];
+
+                    // Set ids
+                    // Binär (i.e. 0000 0011 for motor 1 and 2)
+                    ids = BitConverter.GetBytes((short)3);
+                    for (int i = 0; i < ids.Length; i++) sendData[i + 4] = ids[i];
+
+                    // Set speed
+                    speed = BitConverter.GetBytes(motorSpeed);
+                    for (int i = 0; i < speed.Length; i++) sendData[i + 8] = speed[i];
+
+                    // Set position from database for dxl 1
+                    position = BitConverter.GetBytes((short)motorAngle);
+                    for (int i = 0; i < position.Length; i++) sendData[i + 12] = position[i];
+
+                    // Set position from database for dxl 2
+                    position = BitConverter.GetBytes((short)motorAngle);
+                    for (int i = 0; i < position.Length; i++) sendData[i + 16] = position[i];
+
+                    // Set end of message
+                    msgEnd = BitConverter.GetBytes((short)8888);
+                    for (int i = 0; i < msgEnd.Length; i++) sendData[i + 20] = msgEnd[i];
+
+
+                    Debug.Write("Write to port" + "\n");
+                    _serialPort.Write(sendData, 0, 64);
+
+                    readPosition();
                 }
                 else Debug.Write("Not open" + "\n");
             }
@@ -89,14 +281,24 @@ namespace WriteToComPort
             }
         }
 
+        private void readPosition()
+        {
+            int positionReached = 0;
+            do {
+                positionReached = _serialPort.ReadByte();
+            } while (positionReached != 1);
+
+            textBox_information.Text = "Position is reached";
+        }
+
         private void button_TurnLeft_Click(object sender, System.EventArgs e)
         {
-            rotateMotor("650");
+            setPosition(650, 100);
         }
 
         private void button_TurnMiddle_Click(object sender, System.EventArgs e)
         {
-            rotateMotor("330");
+            setPosition(330, 100);
         }
 
     }
